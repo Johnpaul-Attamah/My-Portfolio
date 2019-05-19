85df8a52435d613c9305ce7040210168b72a7d3c
Johnpaul <bohrplus@gmail.com>
2019-05-19 21:12:04 +0100

    disable hover on mobile

--- resources/js/index.js
+++ resources/js/index.js
@@ -28,3 +28,39 @@ function removeClass(element, _className) {
         }
     }
 }
+
+
+//Remove Hover on mobile Device
+function watchForHover() {
+    let hasHoverClass = false;
+    let container = document.body;
+    let lastTouchTime = 0;
+
+    function enableHover() {
+        // filter events coming from touch events
+        if (new Date() - lastTouchTime < 500) return;
+        if (hasHoverClass) return;
+
+        container.className += ' hasHover';
+        hasHoverClass = true;
+    }
+
+    function disableHover() {
+        if (!hasHoverClass) return;
+
+        container.className = container.className.replace(' hasHover', '');
+        hasHoverClass = false;
+    }
+
+    function updateLastTouchTime() {
+        lastTouchTime = new Date();
+    }
+
+    document.addEventListener('touchstart', updateLastTouchTime, true);
+    document.addEventListener('touchstart', disableHover, true);
+    document.addEventListener('mousemove', enableHover, true);
+
+    enableHover();
+}
+
+watchForHover();
